4087ae734877f2b763435e44508cd0b78f157474
Kinuthia Ndung'u <kinuthia.ndungu@andela.com>
2016-11-10 03:01:49 +0300

    Add React Bootstrap

--- app/index.jsx
+++ app/index.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
+import Bootstrap from 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap';
 
 const Hello = () => (
-  <h1>Hello World</h1>
+  <div>
+    <h1>Hello World</h1>
+    <Button>Click Me!</Button>
+  </div>
 );
 
 render(<Hello />, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -15,14 +15,44 @@ module.exports = {
     publicPath: '/build/'
   },
   module: {
-    loaders: [{
-      test: /\.jsx$/,
-      loader: 'babel-loader',
-      include: path.join(__dirname, 'app'),
-      query: {
-        presets: ['es2015', 'react', 'react-hmre']
+    loaders: [
+      {
+        test: /\.jsx$/,
+        loader: 'babel-loader',
+        include: path.join(__dirname, 'app'),
+        query: {
+          presets: ['es2015', 'react', 'react-hmre']
+        }
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+      },
+      {
+        test: /\.png$/,
+        loader: 'url-loader?limit=100000'
+      },
+      {
+        test: /\.jpg$/,
+        loader: 'file-loader'
+      },
+      {
+        test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url?limit=10000&mimetype=application/font-woff'
+      },
+      {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url?limit=10000&mimetype=application/octet-stream'
+      },
+      {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'file'
+      },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url?limit=10000&mimetype=image/svg+xml'
       }
-    }]
+    ]
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin()
